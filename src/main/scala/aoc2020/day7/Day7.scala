fc0d3a7d779ddab797e03cccafc5f8ae25cd73b5
Hauke Hans <hauke.hans@eventim.de>
2020-12-07 14:07:03 +0100

    remove useless case class

--- src/main/scala/aoc2020/day7/Day7.scala
+++ src/main/scala/aoc2020/day7/Day7.scala
@@ -4,23 +4,42 @@ import aoc2020.Util._
 
 object Day7 {
 
-  case class RawBag(name: String, children: List[(Int, String)])
-
   val SingleBagPattern = raw"(\d) (\w+ \w+) bags?".r
   val Pattern =
     raw"(\w+ \w+) bags contain (\d \w+ \w+ bags?|no other bags)((?:, \d \w+ \w+ bags?)*)\.".r
 
+  private def parseRow(
+      bagName: String,
+      maybeFirst: String,
+      maybeRest: String
+  ): (String, List[(Int, String)]) = {
+
+    val firstChild = maybeFirst.trim match {
+      case "no other bags"             => None
+      case SingleBagPattern(num, name) => Some(num.toInt -> name)
+    }
+
+    val otherChildren = Option(maybeRest).map {
+      _.split(',').tail
+        .map(_.replace(',', ' ').trim)
+        .map {
+          case SingleBagPattern(num, name) => num.toInt -> name
+        }
+        .toList
+    }
+
+    bagName ->
+      firstChild.fold(List()) {
+        List(_) ++ otherChildren.getOrElse(Nil)
+      }
+  }
+
   def main(args: Array[String]): Unit = {
     val bagHierarchy: Map[String, List[(Int, String)]] =
-      readLines("input/day7.txt")
-        .map {
-          case Pattern(bagName, maybeFirst, maybeRest) =>
-            parseRow(bagName, maybeFirst, maybeRest)
-        }
-        .map {
-          case RawBag(name, children) => name -> children
-        }
-        .toMap
+      readLines("input/day7.txt").map {
+        case Pattern(bagName, maybeFirst, maybeRest) =>
+          parseRow(bagName, maybeFirst, maybeRest)
+      }.toMap
 
     def canContainGoldBag(bag: String): Boolean = {
       val children = bagHierarchy.getOrElse(bag, Nil).map(_._2)
@@ -37,33 +56,5 @@ object Day7 {
 
     println(p1)
     println(p2)
-  }
-
-  private def parseRow(
-      bagName: String,
-      maybeFirst: String,
-      maybeRest: String
-  ) = {
-
-    val firstChild = maybeFirst.trim match {
-      case "no other bags"             => None
-      case SingleBagPattern(num, name) => Some(num.toInt -> name)
-    }
-
-    val otherChildren = Option(maybeRest).map {
-      _.split(',').tail
-        .map(_.replace(',', ' ').trim)
-        .map {
-          case SingleBagPattern(num, name) => num.toInt -> name
-        }
-        .toList
-    }
-
-    RawBag(
-      bagName,
-      firstChild.fold(List()) {
-        List(_) ++ otherChildren.getOrElse(Nil)
-      }
-    )
   }
 }
